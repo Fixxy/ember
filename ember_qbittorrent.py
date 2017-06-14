5be36a08608b79967e5fb7a01ae7cc60fdcbd01c
Fixxy <Fixxy@users.noreply.github.com>
2017-06-14 15:12:31 +0300

    temp fix for "referenced before assignment"

--- ember_qbittorrent.py
+++ ember_qbittorrent.py
@@ -1,11 +1,12 @@
 import json
 import requests
+import sys
 
 def qbLogin(s, user, password):
 	try:
 		reqLogin = s.post("http://127.0.0.1:8080/login", data={'username':user, 'password':password})
-	except reqLogin.exceptions.RequestException as e:
-		print(e)
+	except:
+		print("Something went wrong in reqLogin")
 		sys.exit(1)
 	print("   " + str(reqLogin.status_code) + " " + str(reqLogin.reason))
 	return
@@ -14,8 +15,8 @@ def qbSetPreferences(s, hdr, payload):
 	json_data = "json={}".format(json.dumps(payload))
 	try:
 		reqPref = s.post("http://127.0.0.1:8080/command/setPreferences", headers=hdr, data=json_data)
-	except reqPref.exceptions.RequestException as e:
-		print(e)
+	except:
+		print("Something went wrong in reqPref")
 		sys.exit(1)
 	print("   " + str(reqPref.status_code) + " " + str(reqPref.reason))
 	return
@@ -23,8 +24,8 @@ def qbSetPreferences(s, hdr, payload):
 def qbAddMagnet(s, hdr, urls):
 	try:
 		reqAdd = s.post("http://127.0.0.1:8080/command/download", headers=hdr, data={'urls': urls})
-	except reqAdd.exceptions.RequestException as e:
-		print(e)
+	except:
+		print("Something went wrong in reqAdd")
 		sys.exit(1)
 	print("   " + str(reqAdd.status_code) + " " + str(reqAdd.reason))
 	return
